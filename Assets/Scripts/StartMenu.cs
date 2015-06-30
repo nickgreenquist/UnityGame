78f20fa4a40dfca1c8d0734766860d076f70b7b1
nickgreenquist <nickgreenquist@gmail.com>
2015-06-29 22:01:38 -0400

    first commit
    adding the project to git

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour
+{
+
+	public int width = 450;
+	public int height = 150;
+
+	public int trialsWidth = 100;
+	public int trialsHeight = 50;
+
+	public int numTrials = 10;
+	
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+
+
+    }
+}
+
