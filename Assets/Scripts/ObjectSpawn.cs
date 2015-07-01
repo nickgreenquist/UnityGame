aa46c55907a5bc056045238030a15bdee2181084
nickgreenquist <nickgreenquist@gmail.com>
2015-07-01 00:04:12 -0400

    Game Working
    User can input trials
    Game logic works
    Scoring works
    Added basic reaction time feedback

--- Assets/Scripts/ObjectSpawn.cs
+++ Assets/Scripts/ObjectSpawn.cs
@@ -8,6 +8,11 @@ public class ObjectSpawn : MonoBehaviour {
 	public GameObject text1;
 	public GameObject text2;
 	public GameObject text3;
+	public GameObject text4;
+	public GameObject gameover;
+	public Text scoreText;
+	public Text average;
+	public Text performance;
 
 	public InputField input;
 
@@ -19,13 +24,19 @@ public class ObjectSpawn : MonoBehaviour {
 	public GameObject Baseball;
 	Vector3 objectPosition;
 	int randomNum = 0;
-	public int spawnTimer = 0;
-	public int spawnCounter = 0;
+
+
 	public int currentRound;
 	public int rounds;
+	public int numObject;
+	public int rerolls;
+	public bool reRoll;
 
 	public float timer;
 	public float maxTimer;
+	public float score;
+	public float averageReactionTime;
+	public int numCorrectAnswers;
 
 
 	GameObject newObject;
@@ -42,6 +53,7 @@ public class ObjectSpawn : MonoBehaviour {
 
 	bool objectPicked;
 
+	bool userHit;
 
 	void Start () {
 		showingObject = false;
@@ -57,17 +69,46 @@ public class ObjectSpawn : MonoBehaviour {
 		text1 = GameObject.FindGameObjectWithTag("text1");
 		text2 = GameObject.FindGameObjectWithTag("text2");
 		text3 = GameObject.FindGameObjectWithTag("text3");
+		text4 = GameObject.FindGameObjectWithTag("text4");
+		gameover= GameObject.FindGameObjectWithTag("gameover");
+
+		text4.SetActive(false);
 		text3.SetActive(false);
+		gameover.SetActive(false);
+
+		numObject = 1;
+		currentRound = 1;
+		rounds = 2;
+
+		score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		//game over
+		if(currentRound > rounds)
+		{
+			text1.SetActive(false);
+			text2.SetActive(false);
+			text3.SetActive(false);
+			text4.SetActive(false);
+			gameover.SetActive(true);
+			showingObject = false;
+			showingSixObjects = false;
+
+			int scoreInt = (int)(score * 10);
+			scoreText.text = "Score: " + scoreInt.ToString();
+			average.text = "Average Reaction Time: " + (averageReactionTime/numCorrectAnswers).ToString();
+		}
+
+		//game start
 		if(selectingTrials)
 		{
 			if (Input.GetKeyDown ("space"))
 			{
 				rounds = int.Parse(input.text);
+				rerolls = rounds / 4;
 				selectingTrials = false;
 				showingObject = true;
 
@@ -81,6 +122,7 @@ public class ObjectSpawn : MonoBehaviour {
 		//show random object for 2 seconds, then show 6 objects for 2 seconds with 1 second delay
 		if(showingObject)
 		{
+			text4.SetActive(false);
 			if(!objectPicked)
 			{
 			randomNum = Random.Range(1,7);
@@ -143,6 +185,18 @@ public class ObjectSpawn : MonoBehaviour {
 
 		if(showingSixObjects)
 		{
+			text4.SetActive(true);
+			if(numObject >= 6)
+			{
+				numObject = 0;
+				showingObject = true;
+				showingSixObjects = false;
+				objectPicked = false;
+				maxTimer = 2;
+				text3.SetActive(true);
+				currentRound++;
+			}
+
 			if(waitingOneSecond)
 			{
 				Destroy(newObject);
@@ -162,7 +216,9 @@ public class ObjectSpawn : MonoBehaviour {
 				if(!objectPicked)
 				{
 
+
 					randomNum = Random.Range(1,7);
+
 					if(randomNum == 1)
 					{
 						newObject = (GameObject)Instantiate( Soccer,this.gameObject.transform.position,this.gameObject.transform.rotation);
@@ -193,13 +249,31 @@ public class ObjectSpawn : MonoBehaviour {
 						newObject = (GameObject)Instantiate( Baseball,this.gameObject.transform.position,this.gameObject.transform.rotation);
 						type = 6;
 					}
+
+					//okay lets weight the chance of the actual target showing up based on the number of trials selected
+
+					//reroll for every 3 trials selected
+					if(type == correctType)
+					{
+						if(rerolls < rounds / 3)
+						{
+							reRoll = true;
+							rerolls++;
+						}
+					}
+
 					//position the object if needed
 					objectPosition = this.gameObject.transform.position;
 					objectPosition.y += 2;
 					newObject.gameObject.transform.position = objectPosition;
 
-					timer = 0;
-					objectPicked = true;
+					if(!reRoll)
+					{
+						timer = 0;
+						objectPicked = true;
+						rerolls = 0;
+						reRoll = false;
+					}
 
 				}
 
@@ -212,19 +286,28 @@ public class ObjectSpawn : MonoBehaviour {
 						waitingOneSecond = true;
 						maxTimer = 1;
 						type = 0;
+						numObject++;
+						userHit = false;
 					}
 					timer += Time.deltaTime;
 
 					//handle user input
-					if (Input.GetKeyDown ("space"))
+					if (Input.GetKeyDown ("space") && !userHit)
 					{
+						userHit = true;
 						if(type == correctType)
 						{
 							print("correct1");
+							score+= maxTimer - timer;
+							averageReactionTime += timer;
+							numCorrectAnswers++;
+							print (score);
 						}
 						else
 						{
 							print("wrong!");
+							score -= 1;
+							print (score);
 						}
 					}
 				}
